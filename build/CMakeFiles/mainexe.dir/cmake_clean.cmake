d013d534cfe9eade59aab668cba42dbbf540dd66
Vivek Gupta <vg19988@gmail.com>
2019-10-29 10:14:02 +0000

    all

---
+++ build/CMakeFiles/mainexe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainexe.dir/src/main.c.o"
+  "mainexe.pdb"
+  "mainexe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mainexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/CMakeFiles/libmylib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libmylib.dir/Num.c.o"
+  "liblibmylib.pdb"
+  "liblibmylib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libmylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
