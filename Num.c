400351b5933ba4f761a41ddcb94b925a83217572
Vivek Gupta <vg19988@gmail.com>
2019-10-29 11:56:29 +0530

    static and extern added

--- Num.c
+++ Num.c
@@ -1,4 +1,6 @@
 #include "Num.h"
+static int var=10;
+int temp=0;
 int func(int x){
 	x++;
 	x+=2;
@@ -6,6 +8,7 @@ int func(int x){
 }
 
 int details(int y){
+	var=5;
 	int something=1;
 	return something;
 }
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "Num.h"
+
+int xx=5;
+extern int temp;
 int main()
 {
+	temp=4;
 	func(10);
 	details(11);
 } 
